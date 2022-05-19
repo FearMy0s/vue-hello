42763adcefed0b60f121066a746d387cfa054805
FearMy0s <mirko.giannetta96@gmail.com>
2022-05-19 16:50:36 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+
+const ciao = new Vue ({
+    el:`#ciao`,
+    data(){
+        return{
+            bellovue: `ciao vue`,
+            no: "https://lolstatic-a.akamaihd.net/frontpage/apps/prod/rg-kayn-reveal/it_IT/94dcc05587bfb7cf3b581917f3dd6662df5eb212/assets/img/share/kayn_1200x630.jpg"
+        }
+
+    },
+})
+
+    
